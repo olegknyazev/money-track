363a4857096661b99a9e1c59025dac99cfa439ab
Oleg Knyazev <knyazev751@gmail.com>
2018-03-12 15:25:02 +0300

    Add a namespace for working with transactions

---
+++ src/money_track/transaction.clj
@@ -0,0 +1,9 @@
+(ns money-track.transaction
+  (:require [money-track.data :as data]
+            [clojure.java.jdbc :as jdbc]))
+
+(defn get-transactions []
+  (jdbc/query data/db-spec "SELECT * FROM transaction;"))
+
+(defn add-transaction! [t]
+  (jdbc/insert! data/db-spec :transaction t))

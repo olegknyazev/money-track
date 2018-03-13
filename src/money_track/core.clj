a753d62bea81aba4d4337ac46802f8c1e7aec7c6
Oleg Knyazev <knyazev751@gmail.com>
2018-03-13 18:28:43 +0300

    Add the date-from and date-to parameters to /transaction API

--- src/money_track/core.clj
+++ src/money_track/core.clj
@@ -1,14 +1,16 @@
 (ns money-track.core
   (:require [compojure.core :refer [defroutes GET POST]]
             [compojure.route :as route]
-            [ring.middleware.json :as middleware.json]
+            [ring.middleware.params :refer [wrap-params]]
+            [ring.middleware.keyword-params :refer [wrap-keyword-params]]
+            [ring.middleware.json :refer [wrap-json-response wrap-json-body]]
             [ring.util.http-response :as response]
             [money-track.transaction :as tx])
   (:gen-class))
 
 (defroutes app-routes
-  (GET "/transaction" []
-       (response/ok (tx/get-transactions)))
+  (GET "/transaction" req
+       (response/ok (tx/get-transactions (:params req))))
   (POST "/transaction/new" {t :body}
         (if (tx/transaction? t)
           (do
@@ -18,6 +20,8 @@
 
 (def app
   (-> app-routes
-      (middleware.json/wrap-json-response)
-      (middleware.json/wrap-json-body {:keywords? true})))
+      wrap-keyword-params
+      wrap-params
+      wrap-json-response
+      (wrap-json-body {:keywords? true})))
 
--- src/money_track/transaction.clj
+++ src/money_track/transaction.clj
@@ -1,9 +1,16 @@
 (ns money-track.transaction
   (:require [money-track.data :as data]
-            [clojure.java.jdbc :as jdbc]))
+            [clojure.java.jdbc :as jdbc])
+  (:import java.time.LocalDate java.sql.Date))
 
-(defn get-transactions []
-  (jdbc/query data/db-spec "SELECT * FROM transaction;"))
+(defn get-transactions
+  ([] (get-transactions {}))
+  ([params]
+   (let [params (extract-date params)]
+     (jdbc/query data/db-spec
+                 ["SELECT * FROM transaction WHERE date >= ? AND date <= ?;"
+                  (:date-from params)
+                  (:date-to params)]))))
 
 (defn add-transaction! [t]
   (jdbc/insert! data/db-spec :transaction t))
@@ -11,3 +18,10 @@
 (defn transaction? [t]
   (and (map? t)
        (every? #(contains? t %) [:amount :merchant])))
+
+(defn extract-date [params]
+  (assoc params
+         :date-from (Date/valueOf (:date-from params (LocalDate/of 1990 01 01)))
+         :date-to (Date/valueOf (:date-to params (LocalDate/now)))))
+
+
